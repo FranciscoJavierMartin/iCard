95305b685a512697026bef60188f44c51c866f1a
Francisco <javiermartincuenca@gmail.com>
2021-11-04 20:56:12 +0100

    Create user model

--- server/server/settings.py
+++ server/server/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'drf_yasg'
+    'drf_yasg',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# USERS
+AUTH_USER_MODEL = 'users.User'
