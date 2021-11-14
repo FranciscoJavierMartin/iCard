562b9af6056c07efc9524cd7094f80d3a9e120b1
Francisco <javiermartincuenca@gmail.com>
2021-11-14 13:06:17 +0100

    Upload photo to client

--- client/src/components/admin/Category/AddEditCategoryForm/AddEditCategoryForm.tsx
+++ client/src/components/admin/Category/AddEditCategoryForm/AddEditCategoryForm.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Form, Image, Button } from 'semantic-ui-react';
+import { useDropzone } from 'react-dropzone';
+
 import './AddEditCategoryForm.scss';
 
 export default function CategoryForm() {
+  const [previewImage, setPreviewImage] = useState<string>('');
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    const file = acceptedFiles[0];
+    setPreviewImage(URL.createObjectURL(file));
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: 'image/jpeg, image/png',
+    noKeyboard: true,
+    multiple: false,
+    onDrop,
+  });
+
   return (
     <Form className='add-edit-category-form'>
       <Form.Input name='title' placeholder='Category' />
-      <Button type='button' fluid content='Upload image' />
+      <Button
+        type='button'
+        fluid
+        content='Upload image'
+        {...(getRootProps() as any)}
+      />
+      <input {...getInputProps()} />
+      <Image src={previewImage} fluid />
       <Button type='submit' content='Create' primary fluid />
     </Form>
   );
